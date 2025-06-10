f9add82a0b8946d2d09d4d945cbdd2771602e518
sdw98 <sdw9851@gmail.com>
2025-06-10 10:31:31 +0900

    class 이해하기

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,24 @@
-class Person {
-    String name; // 필드(클래스의 변수)
-    int age;
+class Car {
+    String name;
+    String brand;
+    int year;
 
-    public void sayHello() {
-        System.out.println("안녕하세요? 저는 " + name + "입니다. 저의 나이는 " + age + "세 입니다. ");
-    }
-
-    public void work() {
-        System.out.println("일하는중");
+    public void displayInfo() {
+        System.out.println(
+                "이 차는 브랜드가 " + brand + "고 " +
+                "연식은 " + year + "년식인 " + name + "입니다.");
     }
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person(); //객체 or  인스턴스
+        Car car = new Car();
 
-        person.name = "신동원";
-        person.age = 28;
+        car.brand = "벤츠";
+        car.year = 2024;
+        car.name = "G바겐";
 
-        person.sayHello();
-        person.work();
+        car.displayInfo();
     }
 }
